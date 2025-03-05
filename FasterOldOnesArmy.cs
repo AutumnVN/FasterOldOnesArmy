800b3cb235d6b40984cb0073a90d857dc3e1545c
AutumnVN <autumnvnchino@gmail.com>
2025-03-05 20:36:32 +0700

    init

---
+++ FasterOldOnesArmy.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel;
+using Terraria.GameContent.Events;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+
+namespace FasterOldOnesArmy
+{
+    public class FasterOldOnesArmySystem : ModSystem
+    {
+        public override void PostUpdateWorld()
+        {
+            if (DD2Event.TimeLeftBetweenWaves > 1)
+            {
+                DD2Event.TimeLeftBetweenWaves = 1;
+            }
+
+            if (ModContent.GetInstance<FasterOldOnesArmyConfig>().SpawnRate != 1)
+            {
+                DD2Event.LaneSpawnRate = 60 / ModContent.GetInstance<FasterOldOnesArmyConfig>().SpawnRate;
+            }
+        }
+    }
+
+    public class FasterOldOnesArmyConfig : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ServerSide;
+
+        [DefaultValue(4)]
+        [Range(1, 4)]
+        [Slider]
+        public int SpawnRate { get; set; }
+    }
+}
